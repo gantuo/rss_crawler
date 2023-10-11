2c767a899f7309182864ed66b4eaaf6597e788de
gantuo <ganyimeng@gmail.com>
2023-10-11 17:33:33 +0800

    解决36氪、人民网、机核的内容错误问题

--- main.py
+++ main.py
@@ -13,16 +13,52 @@ head = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
 
 
+def from_link(source, link):
+    req = urllib.request.Request(link, headers=head)  # 伪造请求头
+    response = urllib.request.urlopen(req)
+    html = response.read()  # 读取内容
+    try:
+        html = html.decode('utf-8')
+    except:
+        html = html.decode('gbk')
+    soup = BeautifulSoup(html, 'html.parser')
+
+    if source == '机核':
+        tag, attrs = 'span', {'data-text': 'true'}
+    else:
+        tag, attrs = 'p', None
+    text = '\n'.join([item.text for item in soup.find_all(tag, attrs)])
+    return text
+
+
+def from_summary(entry):
+    summary = entry['summary_detail']['value']
+    text = summary.replace('</p><p>', '\n')
+    text = re.sub('<.+>', '', text)
+    return text
+
+
 def parse_entry(source, entry):
     title = entry.title
     link = entry.link
-    req = urllib.request.Request(link, headers=head)  # 伪造请求头
-    response = urllib.request.urlopen(req)
-    html = response.read().decode('utf-8')  # 读取内容
-    soup = BeautifulSoup(html, 'html.parser')
-    text = '\n'.join([item.text for item in soup.find_all('p')])
+
+    if source == '36氪':
+        text = from_summary(entry)
+    else:
+        text = from_link(source, link)
+    text = post_process(text)
     dt = datetime.fromtimestamp(mktime(entry['published_parsed']))
     return {'title': title, 'date': dt.strftime('%Y-%m-%d %H:%M:%S'), 'text': text}
+
+
+def filter_text(text):
+    if len(text) < 100:
+        raise Warning(f'text too short: {text}')
+    m = re.findall('[^\n]+?：.+?\n', text, re.S)
+    if m:
+        matched_text = ''.join(m)
+        if len(matched_text) / len(text) > 0.7:
+            raise Warning(f'match bad pattern: {text}')
 
 
 def parse(source, url):
@@ -30,19 +66,21 @@ def parse(source, url):
     rss = feedparser.parse(url, request_headers=head)
 
     # 获取RSS源的标题、链接、摘要等信息
-    texts = []
+    items = []
     print(f'feed {source} has {len(rss.entries)} articles')
     for entry in rss.entries:
         try:
-            text = parse_entry(source, entry)
-            texts.append(text)
+            item = parse_entry(source, entry)
+            filter_text(item['text'])
+            items.append(item)
         except:
-            traceback.format_exc()
-    return texts
+            print(traceback.format_exc())
+    return items
 
 
 def post_process(text):
-    return re.sub('(\n.?)+', '\n', text).replace(u'\xa0', '')
+    text = text.replace(u'\xa0', '')
+    return re.sub('(\n)+', '\n', text)
 
 
 def process(input):
@@ -68,7 +106,7 @@ def process(input):
     print(f'rss feed done: {source}')
 
 
-if __name__ == '__main__':
+def run_par():
     urls = {
         # entertainment
         # 'Variety':'https://variety.com/feed/',
@@ -101,16 +139,31 @@ if __name__ == '__main__':
         # game
         "机核": "https://www.gcores.com/rss",
         "研游社": "https://www.yystv.cn/rss/feed",
-        "触乐": "https://rsshub.app/chuapp/index/daily",
+        # "触乐": "https://rsshub.app/chuapp/index/daily",
         # animation
-        "005tv": "https://rsshub.app/005tv/zx/latest",
+        # "005tv": "https://rsshub.app/005tv/zx/latest",
     }
 
     from multiprocessing import Pool
 
-    urls.update(urls_cn)
-    # urls = urls_cn
+    # urls.update(urls_cn)
+    urls = urls_cn
     # 进程数
     num_workers = len(urls)
+
     with Pool(num_workers) as p:
         p.map(process, urls.items())
+
+
+def test():
+    # url = ("人民网时政新闻","http://www.people.com.cn/rss/politics.xml")
+    url = ("机核", "https://www.gcores.com/rss")
+    # url = ("36氪", "https://36kr.com/feed-article")
+    # url =("触乐", "https://rsshub.app/chuapp/index/daily")
+
+    process(url)
+
+
+if __name__ == '__main__':
+    run_par()
+    # test()
